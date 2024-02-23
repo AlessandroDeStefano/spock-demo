f1c8b2cc30fb8c542e7664e761b230c717521ed4
Alessandro De Stefano <140831749+AlessandroDeStefano@users.noreply.github.com>
2024-02-23 14:12:19 +0100

    Work of developerA (#1)
    * Adding README
    
    * Adding subject in UserSpecification
    
    ---------
    
    Co-authored-by: Alessandro De Stefano <ales.destefano@reply.it>

--- src/test/groovy/com/example/user/UserSpecification.groovy
+++ src/test/groovy/com/example/user/UserSpecification.groovy
@@ -1,11 +1,13 @@
 package com.example.user
 
 import spock.lang.Specification
+import spock.lang.Subject
 
 class UserSpecification extends Specification {
 
     def "user name is never empty"() {
         given: "a not initialized user"
+        @Subject
         def user = new User()
 
         expect: "it has a default name"
